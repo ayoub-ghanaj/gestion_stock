7ee3b3505887e63d03172ffa6043750ab0937cf0
BuildTools <unconfigured@null.spigotmc.org>
2022-08-29 11:32:44 +0100

    sanitize inputs and secure

--- resources/views/garage/show.blade.php
+++ resources/views/garage/show.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container-fluid padybot">
     <div class="row">
-      <div class="row mt-4">
+      <div class="row mt-4 ">
 
 
         <div class="col">
@@ -36,8 +36,8 @@
         </div>
         </div>
     </div>
-      <div class="row my-4">
-        <div class="col">
+      <div class="row my-4 ">
+        <div class="col ">
           <div class="card">
             <div class="card-header pb-0">
               <div class="row">
@@ -67,7 +67,6 @@
           </div>
         </div>
       </div>
-    </div>
     @if ($link[0]->rank <= 2)
     <div class="row">
       <div class="col-12">
@@ -101,6 +100,7 @@
           </div>
         </div>
       </div>
+    </div>
     </div>
     @endif
   </div>
